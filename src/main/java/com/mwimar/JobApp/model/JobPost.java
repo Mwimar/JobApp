381e8f1053dfcabeb39d00761bda263154295a18
Mark Mwima <markmwimar@gmail.com>
2024-09-03 15:50:32 +0300

    data annotation

--- src/main/java/com/mwimar/JobApp/model/JobPost.java
+++ src/main/java/com/mwimar/JobApp/model/JobPost.java
@@ -1,11 +1,15 @@
 package com.mwimar.JobApp.model;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class JobPost {
     private int postId;
     private String postProfile;
     private String postDesc;
     private int reqExperience;
     private List<String> postTechStack;
+
 }
