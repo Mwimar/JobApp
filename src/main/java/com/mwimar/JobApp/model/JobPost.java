730252d9d0bfba419fc63bd193a0e12865727a1a
Mark Mwima <markmwimar@gmail.com>
2024-09-03 15:52:16 +0300

    component annotation

--- src/main/java/com/mwimar/JobApp/model/JobPost.java
+++ src/main/java/com/mwimar/JobApp/model/JobPost.java
@@ -3,12 +3,14 @@ package com.mwimar.JobApp.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Component
 public class JobPost {
     private int postId;
     private String postProfile;
