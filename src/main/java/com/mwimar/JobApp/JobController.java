8fe71ebdaa0afe60bebfa1c9cef4b9d5497a0472
Mark Mwima <markmwimar@gmail.com>
2024-09-03 15:58:29 +0300

    get mapping

--- src/main/java/com/mwimar/JobApp/JobController.java
+++ src/main/java/com/mwimar/JobApp/JobController.java
@@ -1,18 +1,19 @@
 package com.mwimar.JobApp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JobController {
 
-    @RequestMapping({"/", "home"})
+    @GetMapping({"/", "home"})
     public String home(){
         return "home";
     }
 
-    @RequestMapping("addjob")
+    @GetMapping("addjob")
     public String addJob(){
         return "addjob";
     }
