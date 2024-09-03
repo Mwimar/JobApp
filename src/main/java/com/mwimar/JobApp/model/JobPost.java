6fda8ab07eaaa607641b7ddc8829f8ddc0a3dc42
Mark Mwima <markmwimar@gmail.com>
2024-09-03 15:48:55 +0300

    job post properties

--- src/main/java/com/mwimar/JobApp/model/JobPost.java
+++ src/main/java/com/mwimar/JobApp/model/JobPost.java
@@ -1,4 +1,11 @@
 package com.mwimar.JobApp.model;
 
+import java.util.List;
+
 public class JobPost {
+    private int postId;
+    private String postProfile;
+    private String postDesc;
+    private int reqExperience;
+    private List<String> postTechStack;
 }
