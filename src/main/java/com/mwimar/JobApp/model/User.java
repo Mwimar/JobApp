ac63329c0f4e0f42085853f74deb83d4748cb278
Mark Mwima <markmwimar@gmail.com>
2024-09-06 18:44:36 +0300

    user details

--- src/main/java/com/mwimar/JobApp/model/User.java
+++ src/main/java/com/mwimar/JobApp/model/User.java
@@ -4,4 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class User {
+    private int uid;
+    private String name;
+    private String email;
+    private String password;
 }
