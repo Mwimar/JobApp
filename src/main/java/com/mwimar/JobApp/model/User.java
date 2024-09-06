fa4914cd2f31e484fa0193c5cbb9b04af3130023
Mark Mwima <markmwimar@gmail.com>
2024-09-06 18:45:29 +0300

    to string

--- src/main/java/com/mwimar/JobApp/model/User.java
+++ src/main/java/com/mwimar/JobApp/model/User.java
@@ -40,4 +40,14 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid=" + uid +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
